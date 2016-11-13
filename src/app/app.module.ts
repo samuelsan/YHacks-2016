02eca9203ffab0119237c30d5b5485349ad01c0c
Samuel San <samuelsan@Samuels-MacBook-Pro.local>
2016-11-12 20:31:26 -0500

    added firebase access to transactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { OwlComponent } from './owl.component';
 import { SpeechBubbleComponent } from './speech-bubble.component'
 import { LeafComponent } from './leaf.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBxOQBeAk9iiNwG7YhXdW9Ibdm7sSlA4jM',
+    authDomain: 'yhacks-2016.firebaseapp.com',
+    databaseURL: 'https://yhacks-2016.firebaseio.com',
+    storageBucket: '',
+    messagingSenderId: '233330134232'
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import { LeafComponent } from './leaf.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
